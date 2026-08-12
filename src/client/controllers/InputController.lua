0014bd5736b3faf1320fabc5589741ffe3e874d7
theredcrown159 <aleksandr.zhmur3@gmail.com>
2026-08-12 20:15:17 +0200

    Add InputController

--- src/client/controllers/InputController.lua
+++ src/client/controllers/InputController.lua
@@ -4,11 +4,29 @@ InputController.__index = InputController
 function InputController.new()
 	local self = setmetatable({}, InputController)
 
+	self.PlayContext = game:GetService("ReplicatedStorage"):WaitForChild("Input"):WaitForChild("PlayContext")
+	self.Sprint = self.PlayContext:WaitForChild("Sprint")
+
+	self.connections = {}
+
 	return self
 end
 
-function InputController:ObjectMethod()
-	print(self.Property)
+function InputController:Start()
+	local pressedConnection = self.Sprint.Pressed:Connect(function() end)
+
+	local releasedConnection = self.Sprint.Released:Connect(function() end)
+
+	table.insert(self.connections, pressedConnection)
+	table.insert(self.connections, releasedConnection)
+end
+
+function InputController:Destroy()
+	for _, connection in ipairs(self.connections) do
+		connection:Disconnect()
+	end
+
+	self.connections = {}
 end
 
 return InputController
